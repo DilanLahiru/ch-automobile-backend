ad1faf43c437183618c59cac138e3980568570a9
DilanLahiru <dilanlahiruijse@gmail.com>
2026-04-30 13:05:31 +0530

    feat: implement update and delete product functionality in product controller and routes

--- controllers/productController.js
+++ controllers/productController.js
@@ -56,4 +56,58 @@ const getProducts = async (req, res) => {
     }
 };
 
-module.exports = { createProduct, getProducts };
+// Update Product
+const updateProduct = async (req, res) => {
+    const { id } = req.params;
+    const { supplierId, categoryId, price, quantity, initialStock, minimumStock } = req.body;
+
+    if (!supplierId || !categoryId || !price || !quantity || !initialStock || !minimumStock) {
+        return res.status(400).json({ message: 'Missing required fields' });
+    }
+
+    const supplier = await supplierModel.findById(supplierId);
+    const category = await categoryModel.findById(categoryId);
+
+    if (!supplier || !category) {
+        return res.status(404).json({ message: 'Supplier or Category not found' });
+    }
+
+    try {
+        const product = await productModel.findByIdAndUpdate(id, {
+            supplierId,
+            categoryId,
+            price,
+            quantity,
+            initialStock,
+            minimumStock,
+            categoryName: category.name,
+            supplierName: supplier.name,
+        });
+        res.status(200).json({
+            message: 'Product updated successfully',
+            product,
+        });
+    } catch (error) {
+        console.log('====================================');
+        console.log(error);
+        console.log('====================================');
+        res.status(500).json({ message: 'Error updating product' });
+    }
+};
+
+// Delete Product
+const deleteProduct = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const product = await productModel.findByIdAndDelete(id);
+        res.status(200).json({
+            message: 'Product deleted successfully',
+            product,
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Error deleting product' });
+    }
+};
+
+module.exports = { createProduct, getProducts, updateProduct, deleteProduct };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const {createProduct, getProducts} = require('../controllers/productController');
+const {createProduct, getProducts, updateProduct, deleteProduct} = require('../controllers/productController');
 
 const router = express.Router();
 
 router.post('/create', createProduct);
 router.get('/all', getProducts);
+router.put('/update/:id', updateProduct);
+router.delete('/delete/:id', deleteProduct);
 
 module.exports = router;
